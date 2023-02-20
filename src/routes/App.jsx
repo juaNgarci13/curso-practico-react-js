a024339e4d0b3bd850f91ea1be338e1f88892e29
juaNgarci13 <97314300+juaNgarci13@users.noreply.github.com>
2023-02-20 13:58:07 -0500

    Introduccion alas imagenes a nuestro proyecto junto con el atomic desing, tipos de componentes y svg y png

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -2,12 +2,12 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 //COMPONENTES
+import Home from "../pages/Home";
 import Login from "../pages/Login";
-import Header from "../components/Header";
+import Layout from "../containers/Layout";
 import Orders from "../pages/Orders";
 import Notfound from "../pages/NotFound";
 import Checkout from "../pages/Checkout";
-import ErrorPage from "../pages/ErrorPage";
 import SendEmail from "../pages/SendEmail";
 import MyAccount from "../pages/MyAccount";
 import NewPassword from "../pages/NewPassword";
@@ -19,48 +19,35 @@ import "../styles/global.css";
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Header />,
-    // errorElement: <ErrorPage />,
+    element: <Layout children={<Home />} />,
   },
   {
     path: "login",
     element: <Login />,
-    // errorElement: <ErrorPage />,
   },
   {
     path: "password-recovery",
     element: <PasswordRecovery />,
-    // errorElement: <ErrorPage />,
   },
   {
     path: "send-email",
     element: <SendEmail />,
-    // errorElement: <ErrorPage />,
   },
   {
     path: "new-password",
     element: <NewPassword />,
-    // errorElement: <ErrorPage />,
   },
   {
     path: "account",
     element: <MyAccount />,
-    // errorElement: <ErrorPage />,
   },
-  // {
-  //   path: "sing-up",
-  //   element: <SingUp />,
-  //   // errorElement: <ErrorPage />,
-  // },
   {
     path: "checkout",
     element: <Checkout />,
-    errorElement: <ErrorPage />,
   },
   {
     path: "orders",
     element: <Orders />,
-    errorElement: <ErrorPage />,
   },
   {
     path: "*",
